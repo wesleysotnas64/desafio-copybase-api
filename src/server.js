eac2fc8d0e3a2c887967f3412e525ce2d2b94b46
Wesley Santos <wesleysotnas64@gmail.com>
2024-01-23 00:16:49 -0300

    config: Configurando CORS para acesso do front.

--- src/server.js
+++ src/server.js
@@ -4,6 +4,19 @@ import { DatabasePostgres } from './database-postgres.js'
 const server = fastify();
 const database = new DatabasePostgres;
 
+// Middleware para habilitar CORS
+server.addHook('onRequest', (request, reply, done) => {
+    // Permitir solicitações de qualquer origem
+    reply.header('Access-Control-Allow-Origin', '*');
+    // Permitir os métodos de solicitação especificados (GET, POST, PUT, DELETE)
+    reply.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    // Permitir os cabeçalhos especificados
+    reply.header('Access-Control-Allow-Headers', 'Content-Type');
+  
+    // Continue para a rota ou manipulador seguinte
+    done();
+  });
+
 server.post('/desafio-copybase', async (request, reply) => {
 
     const assinantes = request.body;
